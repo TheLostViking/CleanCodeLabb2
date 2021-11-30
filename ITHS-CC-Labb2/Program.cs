358c1abe2925d5892adf3c50ec254e2e15d1cd6c
Dan Mehlqvist <dan.mehlqvist@gmail.com>
2021-11-30 07:16:06 +0100

    Add project files.

---
+++ ITHS-CC-Labb2/Program.cs
@@ -0,0 +1,13 @@
+﻿using ITHS_CC_Labb2;
+
+OrderService orderService = new OrderService();
+
+Order slowOrder = new Order
+{
+    ID = Guid.NewGuid(),
+    Email = "slowEmail@example.com",
+    Priority = OrderStatus.Normal,
+    TotalPrce = 100,
+};
+
+orderService.ProcessOrder(slowOrder);  
